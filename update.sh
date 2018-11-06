15ffc0c2bfd899be84b5de7bf981231df1111bf3
Lucaiii <31408531+Lucaiii@users.noreply.github.com>
2018-11-06 17:39:10 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/*
-havent worked on it yet. you already know spotify is gonna be the first thing to be signed.
-*/
+if[]; then
+havent worked on it yet. you already know spotify is gonna be the first thing to be signed
+fi
