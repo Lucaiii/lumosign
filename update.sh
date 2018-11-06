dbf4d589c34e5abe64dacac483f72a66998a7ec4
Lucaiii <31408531+Lucaiii@users.noreply.github.com>
2018-11-06 18:22:17 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 
-if[]; then
-havent worked on it yet. you already know spotify is gonna be the first thing to be signed
+cd /Users/owenwatts/Documents/lumosign/
+
+if
+only works for one singular ipa currently which you can set right below this comment
 fi
+
+oneipa="spotfy";
+
+if
+you need to set ipapath equal to a valid ipa. rips.
+fi
+
+echo "declaring variables";
+validipa="/Users/owenwatts/Documents/lumosign/valid.ipa";
+validfolder="/Users/owenwatts/Documents/lumosign/";
+ipastosign="/Users/owenwatts/Documents/lumosign/ipastosign/";
+
+mv {$validpath} {$validfolder}valid.zip;
+validzip="{$validfolder}valid.zip";
+
+unzip {$validzip};
+
+mv {$validpath}ipastosign/{$oneipa}.ipa {$validfolder}ipastosign/{$oneipa}.zip;
+
+oneipazip="{$validfolder}ipastosign/{$oneipa}.zip";
+
+unzip {$oneipa};
+
+cp {$validvolder}valid/Payload/embedded.mobileprovision {&ipastosign}{oneipa}/Payload/embedded.mobileprovision;
+
+zip {&ipastosign}{oneipa};
+
+mv $validfolder}ipastosign/{$oneipa}.zip {$validpath}ipastosign/{$oneipa}.ipa;
+
+echo "done";
