711566f5efbc7dec10de3c0797dfbdaa40e43e1a
Lucaiii <31408531+Lucaiii@users.noreply.github.com>
2018-11-06 19:00:55 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,36 +2,33 @@
 
 cd /Users/owenwatts/Documents/lumosign/
 
-if
-only works for one singular ipa currently which you can set right below this comment
-fi
 
-oneipa="spotfy";
+: only works for one singular ipa currently which you can set right below this comment
 
-if
-you need to set ipapath equal to a valid ipa. rips.
-fi
+oneipa="spotify";
+
+: you need to set ipapath equal to a valid ipa. rips.
 
 echo "declaring variables";
 validipa="/Users/owenwatts/Documents/lumosign/valid.ipa";
-validfolder="/Users/owenwatts/Documents/lumosign/";
-ipastosign="/Users/owenwatts/Documents/lumosign/ipastosign/";
+validpath="/Users/owenwatts/Documents/lumosign";
+ipastosign="/Users/owenwatts/Documents/lumosign/ipastosign";
 
-mv {$validpath} {$validfolder}valid.zip;
-validzip="{$validfolder}valid.zip";
+mv $validipa $validpath/valid.zip;
+validzip="$validpath/valid.zip";
 
-unzip {$validzip};
+unzip $validzip;
 
-mv {$validpath}ipastosign/{$oneipa}.ipa {$validfolder}ipastosign/{$oneipa}.zip;
+mv $validpath/ipastosign/$oneipa.ipa $validpath/ipastosign/$oneipa.zip;
 
-oneipazip="{$validfolder}ipastosign/{$oneipa}.zip";
+oneipazip="$validpath/ipastosign/$oneipa.zip";
 
-unzip {$oneipa};
+unzip $oneipa.zip;
 
-cp {$validvolder}valid/Payload/embedded.mobileprovision {&ipastosign}{oneipa}/Payload/embedded.mobileprovision;
+cp $validpath/Payload/Appvalley.app/embedded.mobileprovision $ipastosign/Payload/Spotify.app/embedded.mobileprovision;
 
-zip {&ipastosign}{oneipa};
+zip $ipastosign/$oneipa;
 
-mv $validfolder}ipastosign/{$oneipa}.zip {$validpath}ipastosign/{$oneipa}.ipa;
+mv $validpath/ipastosign/$oneipa.zip $validpath/ipastosign/$oneipa.ipa;
 
 echo "done";
